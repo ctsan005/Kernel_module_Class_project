45ca264d8b8572c1fc69864f4225698c4caf772a
Chi Chiu Tsang <ctsan005@ucr.edu>
2021-02-25 03:10:09 -0800

    Start to add note

--- kernel_module/src/ioctl.c
+++ kernel_module/src/ioctl.c
@@ -46,6 +46,22 @@
 #include <linux/sched.h>
 #include <linux/kthread.h>
 
+/**
+ * Idea for data structure:
+ * For container:
+ *  1) Need the cid for the container
+ *  2) Need a pointer that point to the data structure of the thread
+ *      2a) Might add another one to point to the end of the process, make it easier for adding another thread to the existing container
+ *  3) Need a pointer to point to the next container.
+ *      3a) if current container is the only one, point back to itself
+ *  
+ * For thread:
+ *  1) Need thread id to know which thread it is. Should be able to extract from task_struct 
+ *  2) cid make it easier to identify which container belong to
+ *  3) The pointer to the next thread within the container
+ *  4) Maybe add a task struct to keep track the thread? But shouldn't be able to read it from current?
+*/
+
 
 /**
  * Deregister the task from the container.
@@ -63,16 +79,51 @@ int resource_container_delete(struct resource_container_cmd __user *user_cmd)
 
 /**
  * Create/Assign a task to the corresponding container.
+ * Idea:
+ * 1) Need to get the container id (which is cid) from the user
+ * 2) cid can be use to check do I already have the container already, if so, add the new process to the container
+ * 3) if new cid, then first create the container and add the new process to the container
+ * 4) Need a way to keep track the container number that is loaded
+ *      4a) might be good to use link list?
+ * 5) A container can also has multiple process within, need another data structer to list all the process within a container
+ *      5a) might be also good to use link list?
+ * 
+ * Step:
+ * 1) extract the cid from user space cmd, check if there is a container for the cid
+ *  1a) If not, create the container, and put the thread into the new container
+ *  1b) If yes, append the thread into the container thread list
+ *  
  */
 int resource_container_create(struct resource_container_cmd __user *user_cmd)
 {
+    // Structure use to keep the information that is pass from the user
     struct resource_container_cmd cmd;
+
+    // copy_from_user return 0 if it is success
     if (copy_from_user(&cmd, user_cmd, sizeof(cmd)))
     {
+        printk(KERN_ERR "copy from user function fail from resource container create");
         return -1;
     }
+
+    // copy from write success, cmd contain the cid from the user
+    
     return 0;
 }
+
+/**
+ * Switch idea: Need to keep it simple. Each of the switch only apply to one container.
+ *  If current selected container only has one thread, switch do nothing for the selected container.
+ *  If the container has more than one thread, make the current runable thread sleep and wake the next thread.
+ * At the end for switch case, update the container pointer to point to the next container to get ready for next switch
+ * 
+ * Downside for this approch:
+ *  Not scalable, as the number of container increase, switch happen in each container decrease
+ *  
+ * Pro:
+ *  Seem to be not too hard to implement
+ * 
+*/
 
 int resource_container_switch(struct resource_container_cmd __user *user_cmd)
 {
