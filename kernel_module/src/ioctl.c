3fd825eee091a585ac3b04b55586df7a53ff24b4
Chi Chiu Tsang <ctsan005@ucr.edu>
2021-02-28 22:35:30 -0800

    updating free

--- kernel_module/src/ioctl.c
+++ kernel_module/src/ioctl.c
@@ -903,6 +903,9 @@ int resource_container_unlock(struct resource_container_cmd __user *user_cmd)
 int resource_container_free(struct resource_container_cmd __user *user_cmd)
 {
     struct resource_container_cmd cmd;
+    container_block* cblock;
+    memory_block* mblock;
+    tid_block* tblock;
     //debug statement
     // printk("resource_container_free start\n"); 
     
@@ -910,6 +913,14 @@ int resource_container_free(struct resource_container_cmd __user *user_cmd)
     {
         return -1;
     }
+
+    cblock = search_all_container_tid(current->pid);
+    mblock = search_memory(cblock,cmd->oid);
+    tblock = search_memory_tid(mblock, current->pid);
+    if(cblock == NULL || mblock == NULL || tblock == NULL){
+        printk(KERN_ERR "Wrong with free function: something is NULL");
+    }
+    memory_remove(cblock, mblock, tblock);
     //debug statement
     // printk("resource_container_free end\n"); 
     return 0;
